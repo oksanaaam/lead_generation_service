ba5be1d09ebceb1e3a37bc9b7f088e4ed70b6d85
name <ksyusha0kubay@gmail.com>
2023-07-03 19:47:31 +0300

    Change registration path

--- interface/urls.py
+++ interface/urls.py
@@ -6,8 +6,8 @@ app_name = "interface"
 
 urlpatterns = [
     path("accounts/login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
-    path("signup/", views.signup_view, name="signup"),
+    path("accounts/logout/", views.logout_view, name="logout"),
+    path("accounts/signup/", views.signup_view, name="signup"),
     path("lead_generator/", views.lead_generator_view, name="lead_generator"),
     path("deploying/", deploying_page, name="deploying"),
 ]
